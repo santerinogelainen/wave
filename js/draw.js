ae3e29e5c283af1923886ec35096ed90bb5a6eb7
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2016-11-13 00:03:50 +0200

    transformed drawing the wave to a function

--- js/draw.js
+++ js/draw.js
@@ -11,33 +11,18 @@ $(window).resize(function(){
   var json;
   //get json settings
   $.get("js/json/wave_settings.json", function(data) {
-    //number of waves
-    var n = data.amount.y;
-    //y offset (for example 50 will add 50px of empy to top)
-    var yOffset = data.full_offset.y;
-    //x offset
-    var xOffset = data.full_offset.x;
-    //create waves
-    while (n > 0) {
-    //set starting point
-    var x = 0 + xOffset, y = 0 + yOffset;
-    //length of waves
-    while (x < data.amount.x) {
-      //move to point
-      ctx.moveTo(x,y);
-      ctx.beginPath();
-      //to do \/
-      ctx.arc(x, y, data.circle_size, 0, 2 * Math.PI);
-      console.log(data.color[2] + 2 * getY(x));
-      ctx.fillStyle = "hsla(" + data.color[0] + ", " + data.color[1] + "%, " + (data.color[2] + 1.3 * getY(x, data.frequency, data.height, data.x_offset)) + "%, " + data.color[3] + ")";
-      ctx.fill();
-      y = getY(x, data.frequency, data.height, data.x_offset, yOffset);
-      console.log("x = " +  x + "; y = " + getY(x, data.frequency, data.height, data.x_offset, yOffset));
-      ctx.closePath();
-      x += data.spacing.x;
-      }
-    yOffset += data.spacing.y;
-    xOffset -= data.x_offset;
-    n--;
-    }
+    draw(
+      data.amount.y,        //numOfWaves
+      data.full_offset.x,   //xOffset
+      data.full_offset.y,   //yOffset
+      data.amount.x,        //waveLength
+      data.height,          //waveHeight
+      data.circle_size,     //circleSize
+      data.frequency,       //frequency
+      data.spacing.x,       //xSpacing
+      data.spacing.y,       //ySpacing
+      data.x_offset,        //shiftX
+      data.y_offset,        //shiftWave
+      data.color            //color
+    );
   });
--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,7 @@
 //    param x === number (int, float, or double), the current x position
 //    param f === number (int, float, or double), frequency of waves or how close together the waves are (best results with 0 < f < 1) bigger number = closer together
 //    param h === number (int, float, or double), height of waves, bigger number = more steep wave
+//    param s === number (int, float, or double), shift wave to left or right
 //    param o === number (int, float, or double), offset waves from top
 
 
@@ -14,4 +15,42 @@ function getY(x, f, h, s, o) {
   //calculate y
   calc = h * Math.cos(f * x + s) + o;
   return calc;
+}
+
+//draw wave
+//  param numOfWaves === int, number of waves
+//  param xOffset === int, offset from left of the canvas (kinda like margin)
+//  param yOffset === int, offset from top of the canvas (kinda like margin)
+//  param waveLength === int, length of the wave
+//  param waveHeight === int, height of the waves
+//  param circleSize === int, size of the circles
+//  param frequency === int, frequency of the waves
+//  param xSpacing === int, spacing between waves x axis
+//  param ySpacing === int, spacing between waves y axis
+//  param shiftX === int, shift circles on x axis left on every wave (wave is identical to previous)
+//  param shiftWave === int, shift the whole wave
+//  param color === ARRAY!!, hsla color values in order. color of the wave (0 => hue, 1 => saturation, 2 => lightness, 3 => alpha)
+function draw(numOfWaves, xOffset, yOffset, waveLength, waveHeight, circleSize, frequency, xSpacing, ySpacing, shiftX, shiftWave, color) {
+  //create waves
+  while (numOfWaves > 0) {
+  //set starting point
+  var x = 0 + xOffset, y = 0 + yOffset;
+  //length of waves
+  while (x < waveLength) {
+    //move to point
+    ctx.moveTo(x,y);
+    ctx.beginPath();
+    //to do \/ make arc great again (add more config possibilities)
+    ctx.arc(x, y, circleSize, 0, 2 * Math.PI);
+    ctx.fillStyle = "hsla(" + color[0] + ", " + color[1] + "%, " + (color[2] + 1.3 * getY(x, frequency, waveHeight, shiftWave)) + "%, " + color[3] + ")";
+    ctx.fill();
+    y = getY(x, frequency, waveHeight, shiftWave, yOffset);
+    console.log("x = " +  x + "; y = " + getY(x, frequency, waveHeight, shiftWave, yOffset));
+    ctx.closePath();
+    x += xSpacing;
+    }
+  yOffset += ySpacing;
+  xOffset -= shiftX;
+  numOfWaves--;
+  }
 }
